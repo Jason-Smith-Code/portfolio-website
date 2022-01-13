efa071ed22198e6a62f52a26fc08b540b07a3822
jason-smith-code <jasonsmithcode@gmail.com>
2022-01-13 15:19:37 +0000

    mobile optimisation

--- Javascript/main.js
+++ Javascript/main.js
@@ -60,199 +60,199 @@ function check_if_in_view() {
                 //console.log("The element is in view port window")
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait1" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, 0) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait2" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait3" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 2) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait4" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 3)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait5" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 4)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait6" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 5)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait7" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 6)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait8" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 7) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait9" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 8)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait10" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 9)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait11" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 10)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait12" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 11)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait13" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 12)
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait14" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 13)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait15" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 14) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait16" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 15)
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait17" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 16) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait18" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 17)
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait19" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 18) 
 
                 setTimeout(function () {
                     $( "#portraitContainer" ).find( "#portrait20" ).css({
-                        '-webkit-transform' : 'translateX(0)',
-                        '-moz-transform'    : 'translateX(0)',
-                        '-ms-transform'     : 'translateX(0)',
-                        '-o-transform'      : 'translateX(0)',
-                        'transform'         : 'translateX(0)'
+                        '-webkit-transform' : 'translateX(0%)',
+                        '-moz-transform'    : 'translateX(0%)',
+                        '-ms-transform'     : 'translateX(0%)',
+                        '-o-transform'      : 'translateX(0%)',
+                        'transform'         : 'translateX(0%)'
                     });
                 }, interval * 19)
                 // animate each | by changing the color
